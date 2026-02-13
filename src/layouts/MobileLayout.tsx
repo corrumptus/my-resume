08dda8fad441f1848abeb104ae90b81010b12c24
corrumptus <lazarinilucas42@gmail.com>
2026-02-13 02:26:01 -0300

    feat: initing the mobile and non mobile layouts segregation

---
+++ src/layouts/MobileLayout.tsx
@@ -0,0 +1,3 @@
+export default function MobileLayout() {
+    return null;
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,14 @@
 import { createRoot } from "react-dom/client";
+import MobileLayout from "./layouts/MobileLayout";
+import NonMobileLayout from "./layouts/NonMobileLayout";
+
+const MAX_MOBILE_WIDTH = 800;
+
+const width = window.innerWidth;
 
 createRoot(document.getElementById('root')!).render(
-  
+  width <= MAX_MOBILE_WIDTH ?
+    <MobileLayout />
+    :
+    <NonMobileLayout />
 );

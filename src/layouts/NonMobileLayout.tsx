3372a18b8059cb3c2a7050723f70fbf6b0562964
corrumptus <lazarinilucas42@gmail.com>
2026-02-16 01:15:40 -0300

    feat: adding the layout based on the orientation config

--- src/layouts/NonMobileLayout.tsx
+++ src/layouts/NonMobileLayout.tsx
@@ -1,19 +1,51 @@
+import Tabs from "../components/nonMobile/Tabs";
+import type { File } from "../main";
+
 export default function NonMobileLayout({
     changeLang,
     theme,
     changeTheme,
     orientation,
-    changeOrientation
+    changeOrientation,
+    files,
+    selectFile,
+    closeFile
 }: {
     changeLang: (lang: "pt-br" | "en") => void,
     theme: string,
     changeTheme: (theme: string) => void,
     orientation: "backwards" | "forwards",
-    changeOrientation: (orientation: "backwards" | "forwards") => void
+    changeOrientation: (orientation: "backwards" | "forwards") => void,
+    files: File[],
+    selectFile: (fileName: string) => void,
+    closeFile: (fileName: string) => void,
 }) {
-    return <>
-        <Tabs orientation={orientation} />
-        <PrimarySideBar  />
-        <Buttons />
-    </>;
+    const openedFiles = files.filter(f => f.isOpen);
+
+    const selectedFile = files.find(f => f.isSelected) as File;
+
+    return orientation === "backwards" ?
+        <>
+            <Tabs
+                orientation={orientation}
+                openedFiles={openedFiles}
+                selectedFile={selectedFile}
+                selectFile={selectFile}
+                closeFile={closeFile}
+            />
+            <PrimarySideBar />
+            <Buttons />
+        </>
+        :
+        <>
+            <Buttons />
+            <PrimarySideBar />
+            <Tabs
+                orientation={orientation}
+                openedFiles={openedFiles}
+                selectedFile={selectedFile}
+                selectFile={selectFile}
+                closeFile={closeFile}
+            />
+        </>;
 }

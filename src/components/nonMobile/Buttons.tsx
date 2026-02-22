758e2e2ae4740413f36695de80f002bf6754bc2b
corrumptus <lazarinilucas42@gmail.com>
2026-02-22 09:12:20 -0300

    feat: creating the buttons layout

---
+++ src/components/nonMobile/Buttons.tsx
@@ -0,0 +1,16 @@
+export default function Buttons() {
+    return <nav>
+        <button>
+            <img src="https://raw.githubusercontent.com/microsoft/vscode-codicons/542ec2a3375b21d42d4b75a995c4feb896aad305/src/icons/files.svg" alt="a file(paper with the top right corner folded) on the top of other file" />
+        </button>
+        <button>
+            <img src="https://raw.githubusercontent.com/microsoft/vscode-codicons/542ec2a3375b21d42d4b75a995c4feb896aad305/src/icons/copilot.svg" alt="the copilot logo" />
+        </button>
+        <button>
+            <img src="https://raw.githubusercontent.com/microsoft/vscode-codicons/542ec2a3375b21d42d4b75a995c4feb896aad305/src/icons/download.svg" alt="a arrow pointing to the bottom" />
+        </button>
+        <button>
+            <img src="https://raw.githubusercontent.com/microsoft/vscode-codicons/542ec2a3375b21d42d4b75a995c4feb896aad305/src/icons/settings-gear.svg" alt="a gear" />
+        </button>
+    </nav>;
+}

aa611de761d98e64a5b4f2a3927585ca575f341d
corrumptus <lazarinilucas42@gmail.com>
2026-02-28 22:38:56 -0300

    feat: adding state for the settings modal and for the side bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import { useState, useEffect } from "react";
 import NonMobileLayout from "./layouts/NonMobileLayout";
 import MobileLayout from "./layouts/MobileLayout";
-import { MAX_MOBILE_WIDTH, type File } from "./main";
+import { MAX_MOBILE_WIDTH } from "./main";
+import type { AvailableLangs, AvailableOrientations, AvailableThemes, File } from "./main";
 import themes from "./utils/themes";
 import resume from "./assets/resume.html?raw";
 
 export default function App() {
 	const [ width, setWidth ] = useState(window.innerWidth);
-	const [ lang, setLang ] = useState<"pt-br" | "en">("pt-br");
-	const [ theme, setTheme ] = useState<keyof typeof themes>("default");
-	const [ nonMobileOrientation, setNonMobileOrientation ] = useState<"backwards" | "forwards">("backwards");
+	const [ lang, setLang ] = useState<AvailableLangs>("pt-br");
+	const [ theme, setTheme ] = useState<AvailableThemes>("default");
+	const [ nonMobileOrientation, setNonMobileOrientation ] = useState<AvailableOrientations>("backwards");
 	const [ files, setFiles ] = useState<File[]>([
 		{
 			name: "currículo.html",
@@ -21,14 +22,33 @@ export default function App() {
 		}
 	]);
 	const [ fileSelectionStack, setFileSelectionStack ] = useState<string[]>(["currículo.html"]);
+	const [ selectedSideBar, setSelectedSideBar ] = useState<"files" | "chat" | undefined>("files");
+	const [ settingsPosition, setSettingsPosition ] = useState<{ x: number, y: number }>();
 
 	useEffect(() => {
-		const ro = new ResizeObserver(() => setWidth(window.innerWidth));
+		const ro = new ResizeObserver(() => {
+			setWidth(window.innerWidth);
+
+			const diff = window.innerWidth - width;
+
+			setSettingsPosition(prev => {
+				if (prev === undefined)
+					return undefined;
+
+				if (nonMobileOrientation === "forwards")
+					return prev;
+
+				return {
+					x: prev.x + diff,
+					y: prev.y
+				};
+			});
+		});
 
 		ro.observe(document.body);
 
 		return () => ro.disconnect();
-	}, []);
+	}, [width, nonMobileOrientation, settingsPosition?.x, settingsPosition?.y]);
 
     useEffect(() => {
 	    document.documentElement.lang = lang;
@@ -45,7 +65,7 @@ export default function App() {
 		});
 	}, [theme]);
 
-    function changeLang(newLang: "pt-br" | "en") {
+    function changeLang(newLang: AvailableLangs) {
         if (newLang === lang)
             return;
 
@@ -160,5 +180,10 @@ export default function App() {
 			changeFileContent={changeFileContent}
 			selectFile={selectFile}
 			closeFile={closeFile}
+			selectedSideBar={selectedSideBar}
+			changeSelectedSideBar={setSelectedSideBar}
+			download={download}
+			settingsPosition={settingsPosition}
+			changeSettingsPosition={setSettingsPosition}
 		/>;
 }

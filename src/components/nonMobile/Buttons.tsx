a3e7bcd14dcabd4b54d87a5f5f6779a3fc7fb79e
corrumptus <lazarinilucas42@gmail.com>
2026-02-28 22:37:56 -0300

    fix: using the correct theme property and updating the function call

--- src/components/nonMobile/Buttons.tsx
+++ src/components/nonMobile/Buttons.tsx
@@ -1,3 +1,5 @@
+import type { MouseEvent } from "react";
+import type { AvailableLangs, AvailableOrientations, AvailableThemes } from "../../main";
 import themes from "../../utils/themes";
 
 export default function Buttons({
@@ -12,17 +14,17 @@ export default function Buttons({
     selectedSideBar: "files" | "chat" | undefined,
     changeSelectedSideBar: (sideBar: "files" | "chat") => void,
     download: () => void,
-    openSettings: (x: number, y: number) => void,
-    orientation: "backwards" | "forwards",
-    lang: "pt-br" | "en",
-    theme: keyof typeof themes
+    openSettings: (e: MouseEvent, pos: { x: number, y: number }) => void,
+    orientation: AvailableOrientations,
+    lang: AvailableLangs,
+    theme: AvailableThemes
 }) {
     return <nav>
         <button
             onClick={() => changeSelectedSideBar("files")}
             title={lang === "pt-br" ? "Arquivos" : "Files"}
             style={selectedSideBar === "files" ? {
-                [orientation === "backwards" ? "borderRightColor" : "borderLeftColor"]: themes[theme].selectedFileColor
+                [orientation === "backwards" ? "borderRightColor" : "borderLeftColor"]: themes[theme].selectedThing
             } : undefined}
         >
             <img src="https://raw.githubusercontent.com/microsoft/vscode-codicons/542ec2a3375b21d42d4b75a995c4feb896aad305/src/icons/files.svg" alt="a file(paper with the top right corner folded) on the top of other file" />
@@ -31,7 +33,7 @@ export default function Buttons({
             onClick={() => changeSelectedSideBar("chat")}
             title={lang === "pt-br" ? "Converse comigo" : "Chat with me"}
             style={selectedSideBar === "chat" ? {
-                [orientation === "backwards" ? "borderRightColor" : "borderLeftColor"]: themes[theme].selectedFileColor
+                [orientation === "backwards" ? "borderRightColor" : "borderLeftColor"]: themes[theme].selectedThing
             } : undefined}
         >
             <img src="https://raw.githubusercontent.com/microsoft/vscode-codicons/542ec2a3375b21d42d4b75a995c4feb896aad305/src/icons/copilot.svg" alt="the copilot logo" />
@@ -43,7 +45,7 @@ export default function Buttons({
             <img src="https://raw.githubusercontent.com/microsoft/vscode-codicons/542ec2a3375b21d42d4b75a995c4feb896aad305/src/icons/download.svg" alt="a arrow pointing to the bottom" />
         </button>
         <button
-            onClick={e => openSettings(e.clientX, e.clientY)}
+            onClick={e => openSettings(e, { x: e.clientX, y: e.clientY })}
             title={lang === "pt-br" ? "Configurações" : "Settings"}
         >
             <img src="https://raw.githubusercontent.com/microsoft/vscode-codicons/542ec2a3375b21d42d4b75a995c4feb896aad305/src/icons/settings-gear.svg" alt="a gear" />

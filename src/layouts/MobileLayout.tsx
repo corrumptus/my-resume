2221f95cddc2cd61a066455f4819b45b82a31f89
corrumptus <lazarinilucas42@gmail.com>
2026-03-08 01:30:49 -0300

    feat: creating the mobile layout

--- src/layouts/MobileLayout.tsx
+++ src/layouts/MobileLayout.tsx
@@ -1,18 +1,75 @@
-import type { AvailableLangs } from "../main";
+import SideBar from "../components/mobile/SideBar";
+import TabHeader from "../components/mobile/TabHeader";
+import Tabs from "../components/mobile/Tabs";
+import type { AvailableLangs, AvailableThemes, File } from "../main";
 
 export default function MobileLayout({
+    lang,
     changeLang,
     theme,
-    changeTheme
+    changeTheme,
+    files,
+    selectedFile,
+    newFile,
+    changeFileContent,
+    selectFile,
+    closeFile,
+    selectedMenu,
+    changeSelectedMenu,
+    download,
+    downloadFile,
+    remove,
+    isOpen,
+    switchIsOpen
 }: {
+    lang: AvailableLangs,
     changeLang: (lang: AvailableLangs) => void,
-    theme: string,
-    changeTheme: (theme: string) => void
+    theme: AvailableThemes,
+    changeTheme: (theme: AvailableThemes) => void,
+    files: File[],
+    selectedFile: File,
+    newFile: (name: string) => void,
+    changeFileContent: (content: string) => void,
+    selectFile: (fileName: string) => void,
+    closeFile: (fileName: string) => void,
+    selectedMenu: "files" | "chat" | "settings",
+    changeSelectedMenu: (menu: "files" | "chat" | "settings") => void,
+    download: () => void,
+    downloadFile: (file: File) => void,
+    remove: (fileName: string) => void,
+    isOpen: boolean,
+    switchIsOpen: () => void
 }) {
+    const openedFiles = files.filter(f => f.isOpen);
+
     return <>
-        <TabHeader />
-        <TabView />
-        <TabVisualizer />
-        <SideBar />
+        <TabHeader
+            openedFiles={openedFiles}
+            selectedFile={selectedFile}
+            selectFile={selectFile}
+            closeFile={closeFile}
+            theme={theme}
+        />
+        <Tabs
+            selectedFile={selectedFile}
+            changeFileContent={changeFileContent}            
+        />
+        <SideBar
+            lang={lang}
+            changeLang={changeLang}
+            theme={theme}
+            changeTheme={changeTheme}
+            files={files}
+            selectedFile={selectedFile}
+            newFile={newFile}
+            selectFile={selectFile}
+            removeFile={remove}
+            isOpen={isOpen}
+            switchIsOpen={switchIsOpen}
+            selectedMenu={selectedMenu}
+            changeSelectedMenu={changeSelectedMenu}
+            download={download}
+            downloadFile={downloadFile}
+        />
     </>;
 }
